aa6ad436b2fa73686f5bcd1a3223e4f2706db544
mastintin <mastintin@gmail.com>
2015-07-13 16:47:56 +0200

    deprecated

--- Cambios en Xcode/backup.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-
-# Backup
-cp /Users/Manuel/Desktop/Info.plist /Users/Manuel/Desktop/Info.plist.bakup
-
-sudo /usr/bin/sed -i .bak 's/XCiPhoneOSCodeSignContext/XCCodeSignContext/' /Users/Manuel/Desktop/Info.plist
-
--- Cambios en Xcode/copiaIdioma.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo cp ./Fivemac.xclangspec /Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/
