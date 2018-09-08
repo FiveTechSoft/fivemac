9eace978283d4319a543b80bd54d4b7191380b96
Antonio Linares <alinares@fivetechsoft.com>
2018-09-08 19:54:19 +0200

    * Minor changes to previous commit

--- samples/build.sh
+++ samples/build.sh
@@ -17,8 +17,7 @@ echo compiling C module...
 #  add -arch ppc -arch i386 for universal binaries
 if [ -d /Applications/Xcode.app ]; then
    SDKPATH=$(xcrun --sdk macosx --show-sdk-path)
-   HEADERS=$SDKPATH
-   gcc -ObjC $1.c -c -I$HEADERS -I./../include -I./../../harbour/include 
+   gcc -ObjC $1.c -c -I$SDKPATH -I./../include -I./../../harbour/include 
 else
    gcc -ObjC $1.c -c -I./../include -I./../../harbour/include
 fi   
